58ef280bccf9bc3907434a9d8a80d44bcbed4f67
Sindi298 <sindiswamsubo71@gmail.com>
2023-10-26 19:20:15 +0200

    hello-world

---
+++ hello-world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+  *main - prints a statement
+  *Return: returns (0)
+  */
+
+int  main(void)
+{
+
+	puts("programming is like building a multiligual puzzle\n");
+	return (0);
+}
+
---
+++ hello-world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+  *main - prints the size of various type based on the computer it is compiled and run on
+  *Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	printf("size of a char: %zu byte(s)\n", sizeof(char));
+
+	printf("size of an int: %zu byte(s)\n", sizeof(int));
+
